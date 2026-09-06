2b435df097ed42c926529696a13aa1ebf3a8cf5b
StrangeLove <purehyacinth@163.com>
2026-09-06 22:36:16 +0800

    optimization algorithm

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void sort(int arr[], int size)
 {
 	int tmp = 0;
+	bool flag = false;
 	for (int i = 0; i < size - 1; ++i)
 	{
 		for (int j = 0; j < size - 1 - i; ++j)
@@ -15,6 +16,11 @@ void sort(int arr[], int size)
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
 			}
+		}
+
+		if (!flag)
+		{
+			break;
 		}
 	}	
 }
